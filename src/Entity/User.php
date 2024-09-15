afec5eeeab85d9419ff03ce48d57b5e142215c8d
Adsy <asimon@ipsosenso.com>
2024-09-15 15:46:10 +0200

    #7 - Responsive

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -33,7 +33,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
-    private ?string $password = null;
+    private string $password;
 
     /**
      * @var Collection<int, Challenge>
